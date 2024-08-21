2ec6b485cd5e60757d5081c3cb4fa5378cb66b4b
sina-byn <sina.sina.bayan@protonmail.com>
2024-08-21 12:47:54 +0330

    vite build process configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/lib.ts'),
+      name: 're-audio',
+      fileName: 're-audio',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json', outDir: 'dist' }),
+  ],
+});
