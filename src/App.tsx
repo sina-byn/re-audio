34238b2bd1ccf9bc6f23cf14ba06e9e3adfec6c8
sina-byn <sina.sina.bayan@protonmail.com>
2024-09-13 13:09:54 +0330

    App.tsx updated to later act as the development playground

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import { Audio } from './components';
 const App = () => {
   return (
     <div>
-      <Audio>{() => null}</Audio>
+      <Audio
+        playlist={[
+          { id: 1, src: '/1.mp3', name: 'for-her-chill' },
+          { id: 2, src: '/2.mp3', name: 'trap-type-beat-rap-instrumental-riff' },
+          { id: 3, src: '/3.mp3', name: 'whip-afro-dancehall' },
+        ]}
+      >
+        {() => null}
+      </Audio>
     </div>
   );
 };
