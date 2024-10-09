23a582dbe2792eb969bd2e5178a20fa81019a44b
sina-byn <sina.sina.bayan@protonmail.com>
2024-10-09 15:52:09 +0330

    docs: landing page updated

--- packages/www/src/pages/index.tsx
+++ packages/www/src/pages/index.tsx
@@ -13,8 +13,8 @@ import Separator from '../components/Separator';
 // * icons
 import {
   IconCode,
+  IconChartBar,
   IconBrandNpm,
-  IconBrandNextjs,
   IconBrandGithub,
   IconAdjustments,
   IconBrandTypescript,
@@ -118,6 +118,13 @@ export default function Home(): JSX.Element {
               <article className='flex items-center gap-4 bg-dark border border-solid border-transparent hover:border-primary rounded-md p-4'>
                 <IconCode size={50} className='shrink-0 text-primary mx-0' />
                 <span className='font-semibold'>Developer-friendly with an intuitive API</span>
+              </article>
+
+              <article className='flex items-center gap-4 bg-dark border border-solid border-transparent hover:border-primary rounded-md p-4'>
+                <IconChartBar size={50} className='shrink-0 text-primary mx-0' />
+                <span className='font-semibold'>
+                  - Includes custom React hookssl for easily creating audio visualizers
+                </span>
               </article>
             </div>
           </div>
