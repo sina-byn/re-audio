92c195a2b99050fccfa3423eed6a5d842de7ebe1
sina-byn <sina.sina.bayan@protonmail.com>
2024-10-16 16:20:06 +0330

    quantization util removed from utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 // * types
-import type { Pixel, ColorChannel } from '../hooks/useCoverPalette';
-
 type TimeChunk = string | number;
 
 export const isNullish = (value: any) => [null, undefined].includes(value);
@@ -27,36 +25,4 @@ export const generateShuffledArray = (length: number = 0): number[] => {
   }
 
   return shuffled;
-};
-
-// prettier-ignore
-export const quantization = (pixels: Pixel[], depth: number, mainChannel: ColorChannel): Pixel[] => {
-  const MAX_DEPTH = 4;
-
-  if (depth === MAX_DEPTH || pixels.length === 0) {
-    const color = pixels.reduce(
-      (prev: Pixel, curr: Pixel) => {
-        prev.r += curr.r;
-        prev.g += curr.g;
-        prev.b += curr.b;
-
-        return prev;
-      },
-      { r: 0, g: 0, b: 0 }
-    );
-
-    color.r = Math.round(color.r / pixels.length);
-    color.g = Math.round(color.g / pixels.length);
-    color.b = Math.round(color.b / pixels.length);
-    return [color];
-  }
-
-  pixels.sort((p1, p2) => p2[mainChannel] - p1[mainChannel]);
-
-  const mid = pixels.length / 2;
-
-  return [
-    ...quantization(pixels.slice(0, mid), depth + 1, mainChannel),
-    ...quantization(pixels.slice(mid + 1), depth + 1, mainChannel),
-  ];
 };
