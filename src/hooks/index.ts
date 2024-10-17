3bb4e4359e605794318a4d457a2c3df246aea021
sina-byn <sina.sina.bayan@protonmail.com>
2024-10-17 11:52:24 +0330

    useVisualizerChart added to hooks entry point

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useVisualizer } from './useVisualizer';
 export { default as useVisualizerFrame } from './useVisualizerFrame';
+export { default as useVisualizerChart } from './useVisualizerChart';
 export { default as useCoverPalette } from './useCoverPalette';
