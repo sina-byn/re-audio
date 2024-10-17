1a8807b0e405fed8e36c221d0160e3607d77533c
sina-byn <sina.sina.bayan@protonmail.com>
2024-10-17 09:35:20 +0330

    useCoverPalette added to hooks entry point

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useVisualizer } from './useVisualizer';
 export { default as useVisualizerFrame } from './useVisualizerFrame';
+export { default as useCoverPalette } from './useCoverPalette';
