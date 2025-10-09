bd7c08eb2efaf3381212ff88bf12dbd39f8eb6c3
sina-byn <sina.sina.bayan@protonmail.com>
2025-10-09 16:09:57 +0330

    docs: CodeBlock doc component inner order updated

--- packages/www/src/components/CodeBlock.tsx
+++ packages/www/src/components/CodeBlock.tsx
@@ -10,13 +10,13 @@ type CodeProps = { code: string; children: React.ReactNode };
 const CodeBlock = ({ code, children }: CodeProps) => {
   return (
     <>
-      <DefaultCodeBlock language='jsx' showLineNumbers>
-        {code}
-      </DefaultCodeBlock>
-
       <BrowserWindow>{children}</BrowserWindow>
 
       <Admonition type='warning'>In case of any issues, try refreshing the page.</Admonition>
+
+      <DefaultCodeBlock language='jsx' showLineNumbers>
+        {code}
+      </DefaultCodeBlock>
     </>
   );
 };
